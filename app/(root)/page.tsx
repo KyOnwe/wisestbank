623e074695f3f40a007582fac4e173b1b0d4793e
Evil Creator <de.evil.creator@gmail.com>
2024-09-05 00:38:18 -0500

    add 'HeaderBox' component and attach to updated Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
-function Home() {
+const Home = () => {
+    const loggedIn = { firstName: 'Wise', lastName: "Ape"}
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName}
+                subtext="Access and manage your accounts and transactions efficiently" />
+            </header>
+        </div>
+    </section>
   )
 }
 
