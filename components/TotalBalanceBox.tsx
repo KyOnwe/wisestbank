0877a8e342aa555ff1fdfcafa69be0e1597bc474
Evil Creator <de.evil.creator@gmail.com>
2024-09-08 22:56:02 -0500

    create and code first chart, per  &

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,6 +1,7 @@
 import { formatAmount } from '@/lib/utils'
 import React from 'react'
-import AnimatedCounter from './AnimatedCounter'
+import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -8,7 +9,7 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/*DoughnutChart*/}
+            <DoughnutChart accounts={accounts}/>
         </div>
 
         <div className='flex flex-col gap-6'>
