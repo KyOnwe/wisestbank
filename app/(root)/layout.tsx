46be7391e6f31b982b0512c1e07a226a03b37236
Evil Creator <de.evil.creator@gmail.com>
2024-09-09 21:31:19 -0500

    setup sidebar with icons

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { fistName: 'Evil', lastName: 'Urge' };
   return (
-   <main>
-     SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+     <Sidebar user={loggedIn} />
      {children}
    </main>
   );
