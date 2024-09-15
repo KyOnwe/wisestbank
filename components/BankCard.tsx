a48ece5459ece63a17750e743b44d4523a6b34ac
Evil Creator <de.evil.creator@gmail.com>
2024-09-15 17:33:16 -0500

    complete initial setup of all pages and links

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -27,7 +27,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     </h2>
                 </div>
                 <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                    **** **** **** **** <span className='text-16'>${account.mask}</span>
+                ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
                 </p>
             </article>
            </div>
@@ -43,6 +43,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
               width={45}
               height={32}
               alt="mastercard"
+              className='ml-5'
             />
            </div>
            <Image
